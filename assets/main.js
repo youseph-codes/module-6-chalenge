2e5c44ee731b0a2db7160c8f73814f8b2adc2b2f
Yousopp <yousephagueye@gmail.com>
2022-10-03 21:26:15 -0400

    created search formulas + classes in main.js

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,28 @@
-let apiKey = "0813b8808a10bc147fa8e0ea7d165313";
+var apiKey = "0813b8808a10bc147fa8e0ea7d165313";
+const savedSearches = [];
 
-console.log(apiKey)
+let searchHistory = function(cityName) {
+    $('.past-search:contains("' + cityName + '")').remove();
+
+    var searchHistoryInput = $("<p>");
+    searchHistoryInput.addClass("previous-search");
+    searchHistoryInput.text(cityName);
+
+    var searchInputBox = $("<div>");
+    searchInputBox.addClass("previous-search-container");
+
+    searchInputBox.append("searchHistoryEntry");
+
+    var searchHistoryBoxEl = $("#search-history-container");
+    searchHistoryBoxEl.append(searchInputBox);
+
+    if (savedSearches.length > 0){
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    $("#search-input").val("");
+};
