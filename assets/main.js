d0b00f402b9de8f306dca59809f83e7fab49078f
Yousopp <yousephagueye@gmail.com>
2022-10-21 23:39:15 -0400

    targeted api's

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,10 @@
-// defining variables
-let APIKey = "0813b8808a10bc147fa8e0ea7d165313"
+// create functions and variables to collect city inputs and target API calls
+const form = document.getElementById('city-form');
+const history = [];
+
+
+// API call information
+let APIKey = "0813b8808a10bc147fa8e0ea7d165313";
+let cap = 1;
+let APILink = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&=${cap}&appid=${APIKey}`;
+
